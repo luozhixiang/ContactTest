bdfd49f3450712d3eca930ab3aca427d16bef686
luozhixiang <luozhixiang82@gmail.com>
2011-12-06 19:39:09 +0800

    css3 to IOS application

--- js/contact.js
+++ js/contact.js
@@ -17,41 +17,37 @@ $(function(){
 	brite.registerDao("Groups",new brite.dao.SQLiteDao("Groups","id",[{column:'name',dtype:'TEXT'}]));
 	brite.registerDao("Users",new brite.dao.SQLiteDao("Users","id",[{column:'name',dtype:'TEXT'},{column:'email',dtype:'TEXT'},{column:'pno',dtype:'TEXT'}]));	
 	brite.registerDao("GroupUser",new brite.dao.SQLiteDao("GroupUser","id",[{column:'group_id',dtype:'INTEGER'},{column:'user_id',dtype:'INTEGER'}]));
+	
 	initData();
+	
+	$("#location-index").click(function(){
+		refreshIndexDiv();
+	});
+	
+});
+function initData(){
+	var dfd1 = brite.dm.remove("Users");
+	var dfd2 = brite.dm.remove("Groups");	
+	var dfd3 = brite.dm.remove("GroupUser");
+			
+	var dfd4 =brite.dm.create("Users",{email:"001@a.com",name:"Mike1",pno:"123456"});
+	var dfd5 =brite.dm.create("Users",{email:"002@a.com",name:"Mike2",pno:"123456"});
+	var dfd6 =brite.dm.create("Users",{email:"003@a.com",name:"Mike3",pno:"123456"});
+	var dfd7 =brite.dm.create("Users",{email:"004@a.com",name:"Mike4",pno:"123456"});
+	var dfd8 =brite.dm.create("Users",{email:"005@a.com",name:"Mike5",pno:"123456"});
+		
+	var dfd9 =brite.dm.create("Groups",{name:"group1"});
+	var dfd10 =brite.dm.create("Groups",{name:"group2"});
 	refreshIndexDiv();
-	window.setTimeout(function(){
+	$.when(dfd1,dfd2,dfd3,dfd4,dfd5,dfd6,dfd7,dfd8,dfd9,dfd10).done(function(){
 		brite.addDataChangeListener("Users",function(daoChangeEvent){
 			refreshUserListDiv();
 		});
 		brite.addDataChangeListener("Groups",function(daoChangeEvent){
 			refreshGroupListDiv();
 		});
-	},1000);
-	
-	$("#location-index").click(function(){
-		refreshIndexDiv();
-	});
-	$("#location-user").click(function(){
-		refreshUserListDiv()
-	});
-	$("#location-group").click(function(){
-		refreshGroupListDiv();
 	});
 	
-});
-function initData(){
-	brite.dm.remove("Users");
-	brite.dm.remove("Groups");	
-	brite.dm.remove("GroupUser");
-			
-	brite.dm.create("Users",{email:"001@a.com",name:"Mike1",pno:"123456"});
-	brite.dm.create("Users",{email:"002@a.com",name:"Mike2",pno:"123456"});
-	brite.dm.create("Users",{email:"003@a.com",name:"Mike3",pno:"123456"});
-	brite.dm.create("Users",{email:"004@a.com",name:"Mike4",pno:"123456"});
-	brite.dm.create("Users",{email:"005@a.com",name:"Mike5",pno:"123456"});
-		
-	brite.dm.create("Groups",{name:"group1"});
-	brite.dm.create("Groups",{name:"group2"});
 }
 function parseRows2Json(rows){
 	var json = [];
@@ -91,39 +87,6 @@ function refreshIndexDiv(){
 	brite.display('IndexComponent',{});	
 }
 
-//-- init page--//
-function initContactListDiv(){
-	
-	refreshUserListDiv();
-	//refreshGroupListDiv();
-	
-}
-
-//  -----open group div ,set the checked status if the group was selected--//
-function addGroupToUser(id){
-	brite.dm.get("Users",id).done(function(user){
-		$('#selectedUname').html(user.name);
-		$('#selectedUid').val(user.id);
-		brite.dm.list("GroupUser",{matchs:{user_id:id}}).done(function(userGroupList){
-			brite.dm.list("Groups").done(function(groupList){
-				var glen = groupList.length;
-				for(var i=0;i<glen;i++){
-					
-					if(isInGroupList(userGroupList,groupList[i])){
-						groupList[i].chkflg='checked';
-					}else{
-						groupList[i].chkflg='';
-					}
-					
-				}
-				$("#group_div").empty();
-				brite.display('GroupComponent',groupList);	
-			});
-		});
-	});
-
-	//$("#group_div_top").show();
-}
 // -- check the check box status--//
 function isInGroupList(list,obj){
 	var glen = list.length;
@@ -134,6 +97,7 @@ function isInGroupList(list,obj){
 	}
 	return false;
 }
+
 function isInGroupArray(list,obj){
 	var glen = list.length;
 	for(var i=0;i<glen;i++){
